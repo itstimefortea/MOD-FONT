420d57acfee7f2cb1aeb19914a3c9613f0a60262
itstimefortea <patcusack@gmail.com>
2025-10-23 12:19:23 +0100

    Fix Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MOD-FONT/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
 })
-
